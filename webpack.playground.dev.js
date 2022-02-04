b1d0099d47da53b670dc5ee1aac013a9dba56879
ivarprudnikov <ivar.prudnikov@gmail.com>
2022-02-04 23:37:36 +0000

    fix playground material example

--- webpack.playground.dev.js
+++ webpack.playground.dev.js
@@ -40,8 +40,7 @@ module.exports = {
 
   // https://webpack.js.org/configuration/dev-server/
   devServer: {
-    port: 8080,
-    writeToDisk: false // https://webpack.js.org/configuration/dev-server/#devserverwritetodisk-
+    port: 8080
   },
 
   // https://webpack.js.org/plugins/html-webpack-plugin/
