71fa56c2c18f89e62a32d8ae16c9c74e9c7af02c
Adel Rashed <adel_rashed@outlook.com>
2022-06-17 16:39:03 -0400

    upgrade to corejs 3

--- babel.config.js
+++ babel.config.js
@@ -1,43 +1,43 @@
 const BABEL_ENV = process.env.BABEL_ENV || process.env.NODE_ENV;
 
 const defaultPlugins = [
-  '@babel/plugin-proposal-class-properties',
-  '@babel/plugin-proposal-object-rest-spread',
+  "@babel/plugin-proposal-class-properties",
+  "@babel/plugin-proposal-object-rest-spread",
   // IE 11 support
-  '@babel/plugin-transform-object-assign'
+  "@babel/plugin-transform-object-assign",
 ];
 
 module.exports = {
   presets: [
     [
-      '@babel/preset-env',
+      "@babel/preset-env",
       {
-        modules: ['cjs', 'test'].includes(BABEL_ENV) ? 'commonjs' : false,
+        modules: ["cjs", "test"].includes(BABEL_ENV) ? "commonjs" : false,
         targets:
-          BABEL_ENV === 'test' ? { node: 'current' } : { browsers: 'defaults' }
-      }
+          BABEL_ENV === "test" ? { node: "current" } : { browsers: "defaults" },
+      },
     ],
-    '@babel/preset-react'
+    "@babel/preset-react",
   ],
   env: {
     cjs: {
       plugins: defaultPlugins,
-      ignore: ['test/**/*.js']
+      ignore: ["test/**/*.js"],
     },
     umd: {
       plugins: defaultPlugins,
-      ignore: ['test/**/*.js']
+      ignore: ["test/**/*.js"],
     },
     es: {
       plugins: [
         ...defaultPlugins,
-        ['@babel/plugin-transform-runtime', { useESModules: true, corejs: 2 }]
+        ["@babel/plugin-transform-runtime", { useESModules: true, corejs: 3 }],
       ],
-      ignore: ['test/**/*.js']
+      ignore: ["test/**/*.js"],
     },
     test: {
       plugins: defaultPlugins,
-      ignore: []
-    }
-  }
+      ignore: [],
+    },
+  },
 };
